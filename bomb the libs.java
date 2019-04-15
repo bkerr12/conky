38c4a6f7b43faf2d80d4f4d01f9a2e01af649577
bkerr12 <49648765+bkerr12@users.noreply.github.com>
2019-04-15 11:05:21 -0500

    Create bomb the libs.java

---
+++ bomb the libs.java
@@ -0,0 +1,9 @@
+public class bombthelibs {
+
+    public static void main(String[] args) {
+	boolean valid = true;
+		while(valid) {
+        System.out.println("lets drop a bomb on the liberals today!");
+		}
+	}
+}
